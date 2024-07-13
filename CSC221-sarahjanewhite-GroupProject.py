8ae503cf990eb21203dfe7a53fbb347df16ef137
sarahjanewhite <sjwhite1@my.waketech.edu>
2024-07-13 09:56:33 -0400

    Update CSC221-sarahjanewhite-GroupProject.py
    Update needed to print first 10 items in table.

--- CSC221-sarahjanewhite-GroupProject.py
+++ CSC221-sarahjanewhite-GroupProject.py
@@ -4,17 +4,24 @@ import csv
 
 url = 'https://en.m.wikipedia.org/wiki/List_of_best-selling_PlayStation_4_video_games'
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
 table = soup.find('table', class_='wikitable')
+data = []
+
+rows = table.find_all('tr')
+for row in rows:
+    cols = row.find_all(['td', 'th'])
+    cols = [col.text.strip() for col in cols]
+    data.append(cols)
+
+print("Printing First 10 Items...")
+for row in data[1:12]:  # This is needed to ignore the headers
+    print(row)
 
 filename = 'CSC221-webscrape-data.csv'
-with open(filename, 'w', newline='') as csvfile:
+with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
     csvwriter = csv.writer(csvfile)
-    rows = table.find('tbody').find_all('tr')
-    for row in rows:
-        cols = row.find_all(['td', 'th'])
-        cols = [col.text.strip() for col in cols]
-        csvwriter.writerow(cols)
+    for row in data:
+        csvwriter.writerow(row)
 
-print(f'CSV file "{filename}" has been created.')
+print(f'\nCSV file "{filename}" has been created.')
